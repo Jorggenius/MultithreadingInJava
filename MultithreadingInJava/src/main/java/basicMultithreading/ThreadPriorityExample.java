1d9025985946833524c932ba17f95ac6c00b69ee
jorge luis <martinezmonajorgeluis@gmail.com>
2025-06-26 16:06:40 -0500

    This is the commit referent to the What is Thread Priority in java? video

---
+++ MultithreadingInJava/src/main/java/basicMultithreading/ThreadPriorityExample.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicMultithreading;
+
+/**
+ *
+ * @author JORGE
+ */
+public class ThreadPriorityExample {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
+        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+        System.out.println(Thread.currentThread().getPriority());
+    }
+}
